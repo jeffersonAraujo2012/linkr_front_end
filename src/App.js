6bcd997e98f2a352e533cb49ccaf5e5f2400dbba
luanmed <luanmede@gmail.com>
2023-03-24 11:27:00 -0300

    fix: queries get post

--- src/App.js
+++ src/App.js
@@ -9,26 +9,31 @@ import UpdateHashtagContext from "./contexts/UpdataHashtagContext";
 import UserTimeline from "./pages/UserTimeline";
 import UpdateUserPage from "./contexts/UpdateUserPage";
 import FollowersContext from "./contexts/FollowersContext";
+import IsFollowingContext from "./contexts/IsFollowingContext";
 
 export default function App() {
   const [userData, setUserData] = useState();
   const [updataHashtags, setUpdataHashtags] = useState(false);
   const [updateUserPage, setUpdateUserPage] = useState(false);
   const [followers, setFollowers] = useState([]);
+  const [isFollowing, setIsFollowing] = useState(false);
+
   return (
     <UpdateUserPage.Provider value={[updateUserPage, setUpdateUserPage]}>
       <UpdateHashtagContext.Provider value={[updataHashtags, setUpdataHashtags]}>
         <AuthContext.Provider value={{ userData, setUserData }}>
           <FollowersContext.Provider value={[followers, setFollowers]}>
-            <BrowserRouter>
-              <Routes>
-                <Route path="/" element={<PaginaPrincipal />} />
-                <Route path="/sign-up" element={<Signup />} />
-                <Route path="/timeline" element={<Timeline />} />
-                <Route path="hashtag/:hashtag" element={<Hashtag />} />
-                <Route path="/user/:id" element={<UserTimeline />} />
-              </Routes>
-            </BrowserRouter>
+            <IsFollowingContext.Provider value={[isFollowing, setIsFollowing]}>
+              <BrowserRouter>
+                <Routes>
+                  <Route path="/" element={<PaginaPrincipal />} />
+                  <Route path="/sign-up" element={<Signup />} />
+                  <Route path="/timeline" element={<Timeline />} />
+                  <Route path="hashtag/:hashtag" element={<Hashtag />} />
+                  <Route path="/user/:id" element={<UserTimeline />} />
+                </Routes>
+              </BrowserRouter>
+            </IsFollowingContext.Provider>
           </FollowersContext.Provider>
         </AuthContext.Provider>
       </UpdateHashtagContext.Provider>
---
+++ src/contexts/IsFollowingContext.js
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+const IsFollowingContext = createContext();
+
+export default IsFollowingContext;
